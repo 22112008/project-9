13e282e3c36c399ede402b01cedc9b0b8d8a2518
22112008 <87270136+22112008@users.noreply.github.com>
2021-07-13 21:18:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if (keyIsDown(DOWN_ARROW)) {
+ background ("red") ;
+}
+if (keyIsDown(UP_ARROW)) {
+  background ("pink")
+}
+
+
+
+
+
+
+
+}
